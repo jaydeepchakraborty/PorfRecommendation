6ea215f3ff86b3a6d0568f3f518cf23a8aff36a6
Jaydeep Chakraborty (Student) <ASUAD\jchakra1@en4102988l.fulton.ad.asu.edu>
2017-03-14 21:25:26 -0700

    First Commit

---
+++ recoprofconst.py
@@ -0,0 +1,30 @@
+import traceback
+from threading import Thread
+#sudo pip3 install -U nltk
+#sudo pip3 install -U numpy
+from nltk.stem import  WordNetLemmatizer
+#sudo pip3 install gensim
+from gensim import corpora, models
+import os
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem.porter import PorterStemmer
+#sudo pip3 install stop-words
+from stop_words import get_stop_words
+
+#sudo pip3 install pyLDAvis
+#sudo pip3 install IPython
+import pyLDAvis
+import pyLDAvis.gensim
+
+#sudo pip3 install wordcloud
+import wordcloud
+
+import matplotlib.pyplot as plt
+
+
+
+
+prof_lst_file_nm = "data/faculty_list.txt"
+prof_topic_file_path = "data/latenteq/"
+prof_pdf_path = "data/papers/"
+prof_topic_vis_file_path = "data/topicvisual/"
